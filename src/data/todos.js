7605b8eb83304ba537f2c45fa512a25c1b847323
Zahra Mdienh <madinehzahra76@gmail.com>
2022-11-13 22:32:10 +0330

    Update todos.js

--- src/data/todos.js
+++ src/data/todos.js
@@ -4,7 +4,7 @@ export const todos = [
     {id: 3, title: 'Buy car', completed: true, dec: 'import', tag: 'Travel', active: false},
     {id: 4, title: 'Buy pool tickets', completed: false, dec: 'import', tag: 'Life', active: false},
     {id: 5, title: 'Buy pen', completed: true, dec: 'import', tag: 'Work', active: false},
-    {id: 6, title: 'Blass tomorrow 8am', completed: false, dec: 'import', tag: 'Education', active: false},
+    {id: 6, title: 'Class tomorrow 8am', completed: false, dec: 'import', tag: 'Education', active: false},
     {id: 7, title: 'Buy ball', completed: false, dec: 'import', tag: 'Sport', active: false},
     {id: 8, title: 'Buy concert tickets', completed: true, dec: 'import', tag: 'Life', active: false},
     {id: 9, title: 'Blass 4pm', completed: true, dec: 'import', tag: 'Education', active: false},
